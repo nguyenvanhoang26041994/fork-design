f493813e2e4909533e567449a8a41c7bc2f85323
hoang.nguyenvan <hoang.nguyenvan@employmenthero.com>
2022-05-28 16:13:24 +0700

    Migrate to TS

---
+++ src/core/Radio/useRadioGroup.ts
@@ -0,0 +1,12 @@
+import React, { useState, useCallback } from 'react';
+
+const useRadioGroup = (defaultValue: string | number | string[] | undefined) => {
+  const [value, setValue] = useState(defaultValue);
+  const onChange = useCallback((event: React.ChangeEvent<HTMLInputElement>): void => {
+    setValue(event.target.value);
+  }, [setValue]);
+
+  return [{ value, onChange, setValue }];
+};
+
+export default useRadioGroup;
---
+++ src/core/Switch/useSwitch.ts
@@ -0,0 +1,13 @@
+import React, { useState, useCallback } from 'react';
+
+const useSwitch = (defaultValue: boolean | undefined) => {
+  const [checked, setChecked] = useState(defaultValue);
+  const onChange = useCallback((event: React.ChangeEvent<HTMLInputElement>): void => {
+    setChecked(event.target.checked);
+  }, [setChecked]);
+  const toggle = useCallback(() => setChecked(val => !val), [setChecked]);
+
+  return [{ checked, onChange, toggle, setChecked }];
+};
+
+export default useSwitch;
